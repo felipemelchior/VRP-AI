3ee64971a09e97988856549b27edd06a653a1691
homdreen <fehmel@gmail.com>
2018-06-02 11:55:31 -0300

    Adicionado mais comentarios

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-###########################################################
-#           TRABALHO INTELIGENCIA ARTIFICIAL              #
-#          https://github.com/homdreen/vrp-ai             #
-#                                                         #
-#                FELIPE HOMRICH MELCHIOR                  #
-#                      161150758                          #
-########################################################### 
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+###################################################################
+#               TRABALHO INTELIGENCIA ARTIFICIAL                  #
+#              https://github.com/homdreen/vrp-ai                 #
+#                                                                 #   
+#     		 ARTIGO DE REFERENCIA (PROFESSOR SCHEPKE)             # 
+#   http://www.sirc.unifra.br/arquivos/edicoes/2004/Artigo24.pdf  #
+#                                                                 #
+#                  FELIPE HOMRICH MELCHIOR                        #
+#                        161150758                                #
+################################################################### 
 
 from vrp import * # Importação da classe que está em outro arquivo
 
--- vrp.py
+++ vrp.py
@@ -1,7 +1,11 @@
 import math # Importação da biblioteca matematica
 from operator import itemgetter # Importação do itemgetter, usado para ordenar os savings
 
+# Abaixo temos a definição da classe principal, que chamamos de Vrp
+# Nela teremos os métodos que irão calcular a distancia, imprimir dados na tela, calcular economias e calcular o custo
 class Vrp(): # Definição da classe principal do programa, chamada vrp
+
+	# Construtor padrão que inicia algumas listas que serão usadas e também dois numeros inteiros
 	def __init__(self): # Construtor da classe, apenas inicializa o grafo com uma lista vazia
 		self.numConsumers = 0 # Inteiro que guarda o numero de consumidores
 		self.vehiclesCapacity = 0  # Inteiro que guarda a capacidade dos caminhões
@@ -12,25 +16,28 @@ class Vrp(): # Definição da classe principal do programa, chamada vrp
 		self.Routes = [] # Lista de Rotas que os caminhões farão
 		self.Savings = [] # Lista de Economias entre as rotas
 
+	# Leitor dos dados, recebe uma linha por vez de dados e remapeia para as variaveis da classe
 	def reader(self): # Função que faz a leitura dos dados, podendo entrar pelo teclado ou redirecionando a entrada padrão
-		string = input("Enter the number of consumers and th capacity of vehicles => ") # Usado uma string auxiliar para pegar os dados digitados
+		# string = input("Enter the number of consumers and th capacity of vehicles => ") # Usado uma string auxiliar para pegar os dados digitados
+		string = input("") # Mesmo código acima, porém não imprimindo nada na tela
 		self.numConsumers, self.vehiclesCapacity = map(int,string.split()) # Utilizando um map, os dados são convertidos para inteiro e a string é "separada" para as outras variaveis
 
-		string = input("Enter the coords (X, Y) of deposit => ") # Utilizando o mesmo sistema de cima, é utilizado uma string para guardar os dados
+		# string = input("\nEnter the coords (X, Y) of deposit => ") # Utilizando o mesmo sistema de cima, é utilizado uma string para guardar os dados
+		string = input("") # Mesmo código acima, porém não imprimindo nada na tela
 		coord_X, coord_Y = map(int, string.split()) # E o mesmo map é usado para atribuir os dados para outras variaveis
-		print() # Print apenas para saltar um linha
-
+		
 		self.Graph.append([coord_X, coord_Y]) # O deposito é adicionado ao grafo, com as coordenadas que foram passadas e demanda 0
 		self.Demands.append([0]) # O deposito tem demanda 0
 
 		for i in range(self.numConsumers): # Aqui temos um bloco de repetição, que vai de 0 até a quantidade de consumidores passado para o programa
-			string = input("Enter the coords (X, Y) of consumer and the consumer demand => " ) # String auxiliar
+			# string = input("\nEnter the coords (X, Y) of consumer and the consumer demand => " ) # String auxiliar
+			string = input("") # Mesmo código acima, porém não imprimindo nada na tela
 			coord_X, coord_Y, demand = map(int, string.split()) # Novas variaveis atribuidas, da mesma forma de cima
-			print() # Print apenas para saltar um linha
-
+			
 			self.Graph.append([coord_X, coord_Y]) # Consumidor adicionado ao grafo, com suas coordenadas
 			self.Demands.append([demand]) # Cada consumidor é adicionado à lista de demandas com a sua respectiva demanda
 
+	# Abaixo teremos um seção de códigos que irão imprimir dados na tela, usados apenas para debug, pois algumas nem estão sendo chamadas
 	def printGraph(self): # Função que imprime todo o grafo
 		print() # Print apenas para saltar um linha
 		for i in self.Graph: # Bloco de repetição que inicia em 0 e vai até o tamanho total do grafo
@@ -56,6 +63,7 @@ class Vrp(): # Definição da classe principal do programa, chamada vrp
 		for i in self.Savings: # Bloco que percorre toda a lista de economias
 			print(i) # Imprime cada economia
 
+	# Aqui temos uma função que calcula a distancia euclidiana entre todos os pontos através da formula raiz( (xf-xi)² + (yf-yi)² )
 	def euclidianDist(self): # Função que calcula a distancia euclidiana de cada ponto para todos os pontos
 		for i in range(len(self.Graph)): # Repete ate passar por todas as cidades
 			self.Distances.append([]) # Adiciona a lista que sera modificada pela funcao no momento
@@ -67,6 +75,9 @@ class Vrp(): # Definição da classe principal do programa, chamada vrp
 					distY = (self.Graph[i][1] - self.Graph[j][1]) ** 2 # Y final menos Y inicial ao quadrado
 					self.Distances[i].append(math.sqrt(distX + distY)) # Raiz quadrada da soma das distancias em X e distancias em Y
 
+	# Abaixo temos a heurística do programa, usei o Algoritmo de Savings que encontrei em um artigo do professor Schepke
+	# Basicamente este algoritmo encontra as melhores ligações para as rotas
+	# Usando a formula Sij = Ci0 + C0j - Cij
 	def savingsAlgorithm(self): # Função do algoritmo de Economias, este algoritmo leva em conta a distancia de uma cidade i ate chegar ao deposito somado à distancia do deposito ate a cidade j e tudo isso subtraido com a distancia de i para j --> Sij = Ci0 + C0j - Cij
 		saving = 0 # Variavel local para o calculo
 		for i in range(1, len(self.Demands)): # Inicia as Rotas inicialmente apenas com o indice, pois sabemos que cada rota começa em 0 e termina em 0
@@ -108,6 +119,7 @@ class Vrp(): # Definição da classe principal do programa, chamada vrp
 			self.Routes[i].insert(0, 0) # Insere o deposito no inicio
 			self.Routes[i].insert(len(self.Routes[i]), 0) # Insere o deposito no final
 
+	# Com as rotas prontas, esta função é chamada apenas para calcular o custo total de todas as rotas
 	def calcCosts(self): # Função que calcula o custo das rotas
 		for i in range(len(self.Routes)): # For que percorre as rotas
 			for j in range(len(self.Routes[i])-1): # For que percorre as cidades da rota acima
@@ -115,10 +127,11 @@ class Vrp(): # Definição da classe principal do programa, chamada vrp
 				
 		print("\nTotal Cost: ", round(self.Cost,3)) # Imprime o custo
 
+	# Função que apenas faz a chamada de outras funções, usada apenas para diminuir a quantidade de funções chamadas pelo arquivo Main.py
 	def start(self): # Função mestre, que chama todas as outras
 		self.reader() # Chamada da função do leitor
 		self.euclidianDist() # Chamada da função que calcula a distancia euclidiana das cidades
 		self.savingsAlgorithm() # Chamada da função que calcula as economias e gera as rotas
-		print("============================== RESULTS ==============================") # Print de separação
+		# print("============================== RESULTS ==============================") # Print de separação
 		self.printRoutes() # Chamada da função que imprime as rotas
 		self.calcCosts() # Chamada da função que calcula o custo das rotas
